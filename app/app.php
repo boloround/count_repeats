53fc388b99842deebc14cda982d1a793888d006c
Joe Hartman <joe.hartman@yahoo.com>
2015-08-14 15:30:45 -0700

    Complete app.php file, add twig files.

---
+++ app/app.php
@@ -0,0 +1,24 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    use Symfony\Component\Debug\Debug;
+
+    Debug::enable();
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.html.twig');
+    });
+
+    $app->get("/result", function() use ($app) {
+        $this_RepeatCounter = new RepeatCounter;
+        $this_count = $this_RepeatCounter->countRepeats(&_GET['word'], $_GET['string']);
+        return $app['twig']->render('result.html.twig', array('values' => $this_count));
+    })
+
+    return $app;
+ ?>
